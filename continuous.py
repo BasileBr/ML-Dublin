a9cdbe8521190d42d147f86c646d416c82f841ec
FayssalM <fayssalmoumni@gmail.com>
2018-04-23 23:03:30 +0200

    stats dqr continuous

--- continuous.py
+++ continuous.py
@@ -8,16 +8,14 @@ class Continuous:
         
         if fileCSV is None:
             self.pathBank = './dataset/dataset.csv'
-            self.fileCSV = pd.read_csv(filepath_or_buffer=self.pathBank,delimiter = ',', header=0, index_col=1)
+            self.fileCSV = pd.read_csv(filepath_or_buffer=self.pathBank,delimiter = ',', header=0, index_col=0)
         else:
             self.fileCSV = fileCSV
             
         self.continuous = self.fileCSV.select_dtypes(include=[np.number])
         self.pathFileResult = './results/DQR-ContinuousFeatures.csv';
         
-    def get_continuous(self):
-        print("continuous : ",self.continuous)
-        
+    def get_continuous(self):        
         if self.continuous is not None:
             return self.continuous
         else:
@@ -28,20 +26,29 @@ class Continuous:
         
     def draw_DQR(self):
         
-        for feature in self.get_continuous().items():
+        continuous_columns = self.continuous;  
+        continuous_header = ["Features","Count","% Miss", "Card", "Min", "1st Qrt", "Mean","Median","3rd Qrt", "std" ]
+        continuous_features_table = []
+        
+        for col in continuous_columns:
+            feature = [col]
+            feature.append(self.continuous[col].size)
+            feature.append((self.continuous[col].isnull().sum()/self.continuous[col].size) * 100)
+            feature.append(self.continuous[col].unique().size)
+            feature.append(np.min(self.continuous[col]))
+            feature.append(np.percentile(self.continuous[col],25))
+            feature.append(np.mean(self.continuous[col]))
+            feature.append(np.percentile(self.continuous[col],50))
+            feature.append(np.percentile(self.continuous[col],75))
+            feature.append(np.std(self.continuous[col]))
+                
+            continuous_features_table.append(feature);
+
+        print(continuous_features_table);
             
-            feature_name = feature[0]
-            print(feature[0])
-#            count
-#            miss_pourcentage
-#            cardinality
-#            min_value
-#            first_quarter
-#            mean
-#            median
-#            third_quarter
-#            max_value
-#            std_dev
+
 
 cont = Continuous();
+#cont.write_results();
+#cont.get_continuous();
 cont.draw_DQR();
