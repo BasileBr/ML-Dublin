c67f675e55e076aa0059f393d1e24d50d4e8644e
FayssalM <fayssalmoumni@gmail.com>
2018-04-23 21:07:00 +0200

    base

--- continuous.py
+++ continuous.py
@@ -7,13 +7,14 @@ class Continuous:
     def __init__(self,fileCSV=None):
         
         if fileCSV is None:
-            self.pathBank = './'
-            self.fileCSV = pd.read_csv(filepath_or_buffer=self.pathBank,delimiter = ';', header=0, index_col=0)
+            self.pathBank = './dataset/dataset.csv'
+            self.fileCSV = pd.read_csv(filepath_or_buffer=self.pathBank,delimiter = ',', header=1, index_col=1)
         else:
             self.fileCSV = fileCSV
             
         self.continuous = self.fileCSV.select_dtypes(include=[np.number])
-        self.pathFileResult = './Data/Results/DQR-ContinuousFeatures.csv'
+        self.pathFileResult = './results/DQR-ContinuousFeatures.csv';
+        print(self.fileCSV)
     
     def get_continuous(self):
         print("continuous : ",self.continuous)
@@ -29,56 +30,13 @@ class Continuous:
     
     def draw_DQR(self):
         
-        tableContinuous = self.get_continuous()
-        for feature,value in tableContinuous.items():                
-            print(value)
-            #print(feature)
-#            count = len(value)
-#            ERROR HERE TO FIX TO SAVE DATA IN CSV IN FUTUR
-#            nbMiss = 0
-#            for i in range(count):
-#                val = value[i]
-#                if val == ' ?':
-#                    nbMiss += 1
-#                    self.setMissingFeatures('Miss',value)
-#            #print((nbMiss / count) * 100)
-#            
-#
-            unique_value = len(set(tableContinuous))
-            #print(unique_value)
-            # Create & save plots
-            #print(value)
-            '''
-            if unique_value >= 10:
-                
-                ply.offline.plot({
-                    "data": [
-                        ply.graph_objs.Histogram(
-                            x=tableContinuous[feature]
-                        )
-                    ],
-                    "layout": ply.graph_objs.Layout(
-                        title="Histogram of feature \"" + feature + "\" - cardinality >=10"
-                    )
-                }, filename="./Data/HTML/Continuous/%s.html" % feature)
-            else:
-                ply.offline.plot({
-                    "data": [
-                        ply.graph_objs.Bar(
-                            x=tableContinuous[feature].value_counts().keys(),
-                            y=tableContinuous[feature].value_counts().values
-                        )
-                    ],
-                    "layout": ply.graph_objs.Layout(
-                        title="Bar plot for feature \"" + feature + "\" - cardinality <10"
-                    )
-                }, filename="./Data/HTML/Continuous/%s.html" % feature)
+        tableContinuous = self.get_continuous()          
                     
-                    '''
+        
     def launch_test(self):
         csvFile = self.get_csv_file()
         #self.write_results()
         
         self.draw()
         
-Continuous().launch_test()
+Continuous();
