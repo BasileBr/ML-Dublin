f388de1718005b95e8c6349531bc1c57776a403a
Unknown <basile.bruhat@etudiant.univ-rennes1.fr>
2018-04-24 18:46:43 +0200

    update diag bar + histo

--- scrypt.py
+++ scrypt.py
@@ -5,7 +5,7 @@ Created on Mon Apr 23 20:43:12 2018
 @author: Basile Bruhat
 """
 
-
+'''
 import plotly
 import plotly.graph_objs
 
@@ -31,15 +31,32 @@ py.iplot(data, filename='basic histogram')
 
 '''
 
-import plotly
-"""from plotly.graph_objs import Scatter, Layout"""
-import plotly.graph_objs
+' Code pour faire un histogramme'
 
-plotly.offline.init_notebook_mode(connected=True)
+import plotly.offline as offline
+import plotly.graph_objs as go
 
-plotly.offline.plot({
-    "data": [plotly.graph_objs.Bar(x=[1, 2, 3, 4], y=[4, 3, 2, 1])],
-    "layout": Layout(title="hello world")
-})
+
+offline.init_notebook_mode()
+
+offline.plot({'data': [{'x':[10,11,16,17]}],
+               'layout': {'title':'hello world',
+                          'xaxis':{'title':'X axis'},
+                          'yaxis':{'title':'Y axis'},
+                          'font':dict(size=16)}},
+
+            image='png')
+
+' Code pour faire un diagramme bar'
+
+'''
+import plotly 
+"""from plotly.graph_objs import Scatter, Layout""" 
+import plotly.graph_objs 
+plotly.offline.init_notebook_mode(connected=True) 
+plotly.offline.plot({ 
+    "data": [plotly.graph_objs.Bar(x=[1, 2, 3, 4], y=[4, 3, 2, 1])], 
+    "layout": Layout(title="hello world") 
+})'''
     
     
