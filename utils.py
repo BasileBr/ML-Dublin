e0820c41141435a0e593e18a3e5135a85e200eaa
julienleguillant <leguillant.julien@gmail.com>
2018-04-25 19:29:34 +0100

    add utils.py

---
+++ utils.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 25 18:43:21 2018
+
+@author: julienleguillant
+"""
+
+def getHeader():
+        return {
+            'AAGE': 'age',
+            'ACLSWKR': 'class of worker',
+            'ADTIND': 'industry code',
+            'ADTOCC': 'occupation code',
+            'AGI': 'adjusted gross income',
+            'AHGA': 'education',
+            'AHRSPAY': 'wage per hour',
+            'AHSCOL': 'enrolled in edu inst last wk',
+            'AMARITL': 'marital status',
+            'AMJIND': 'major industry code',
+            'AMJOCC': 'major occupation code',
+            'ARACE': 'mace',
+            'AREORGN': 'hispanic Origin',
+            'ASEX': 'sex',
+            'AUNMEM': 'member of a labor union',
+            'AUNTYPE': 'reason for unemployment',
+            'AWKSTAT': 'full or part time employment stat',
+            'CAPGAIN': 'capital gains',
+            'CAPLOSS': 'capital losses',
+            'DIVVAL': 'divdends from stocks',
+            'FEDTAX': 'federal income tax liability',
+            'FILESTAT': 'tax filer status',
+            'GRINREG': 'region of previous residence',
+            'GRINST': 'state of previous residence',
+            'HHDFMX': 'detailed household and family stat',
+            'HHDREL': 'detailed household summary in household',
+            'MARSUPWT': 'instance weight',
+            'MIGMTR1': 'migration code-change in msa',
+            'MIGMTR3': 'migration code-change in reg',
+            'MIGMTR4': 'migration code-move within reg',
+            'MIGSAME': 'live in this house 1 year ago',
+            'MIGSUN': 'migration prev res in sunbelt',
+            'NOEMP': 'num persons worked for employer',
+            'PARENT': 'family members under 18',
+            'PEARNVAL': 'total person earnings',
+            'PEFNTVTY': 'country of birth father',
+            'PEMNTVTY': 'country of birth mother',
+            'PENATVTY': 'country of birth self',
+            'PRCITSHP': 'citizenship',
+            'PTOTVAL': 'total person income',
+            'SEOTR': 'own business or self employed',
+            'TAXINC': 'taxable income amount',
+            'VETQVA': 'fill inc questionnaire for veterans admin',
+            'VETYN': 'veterans benefits',
+            'WKSWORK': 'weeks worked in year'
+        }
+    
+    
+my_dict = getHeader();
+print(my_dict['TAXINC']);
