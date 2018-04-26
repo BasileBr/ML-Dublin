906d9cd4db806967531b27b917b6995ad550771e
Hugo Jové <hugo.jove2@gmail.com>
2018-04-26 14:10:38 +0200

    merging dqr

--- categorical.py
+++ categorical.py
@@ -1,77 +1,85 @@
-#Julien LE GUILLANT IMR2
 import numpy as np
 import pandas as pd
 import collections
 
-class Categorical:
+class CategoricalV2:
     
-#   Init    
-    def __init__(self,fileCSV=None):
+    def __init__(self,fileCSV=None, headerLabels=None):
         
         if fileCSV is None:
             self.pathBank = './dataset/dataset.csv'
-            self.fileCSV = pd.read_csv(filepath_or_buffer=self.pathBank,delimiter = ',', header=0, index_col=1)
+            self.fileCSV = pd.read_csv(filepath_or_buffer=self.pathBank,delimiter = ',', header=0, index_col=0);
         else:
-            self.fileCSV = pd.read_csv(filepath_or_buffer=fileCSV,delimiter = ',', header=0, index_col=0);
-            
+            self.fileCSV = pd.read_csv(filepath_or_buffer=fileCSV, delimiter = ';', header=0, names= headerLabels);
+       
         self.categorical = self.fileCSV.select_dtypes(exclude=[np.number]);
-        self.pathFeatures = './results/categorical-features.csv';
-        self.pathDQR = './results/E-DQR-categorical.csv';
-        self.__categorical_features_table = [];
+        self.pathFeatures = './results/-features.csv';
+        self.pathDQR = './results/E-DQR-categorical.csv';    
     
-    def get_categorical(self):
-        
-        if self.categorical is not None:
-            return self.categorical;
-        else:
-            return pd.read_csv(filepath_or_buffer=self.pathFeatures);
-        
-#   Write data in csv file from tab    
+
+    
     def write_results(self, table=None, header_columns=None, path=None):
-        
         if table is None and path is None:
             pd.DataFrame(self.categorical).to_csv(path_or_buf=self.pathFeatures);
         else:
-            pd.DataFrame(table).to_csv(path_or_buf=path, header = header_columns, index = False);
-        
-             
-    def draw_DQR(self):
-        categorical_header = ["Feature name","Count","% Miss", "Card", "Mode", "Mode Freq", "Mode %","2nd Mode","2nd Mode Freq","2nd Mode %"]
-        categorical_columns = self.get_categorical();
-        
-        for cat_name in categorical_columns:   
-            
-            if cat_name is id:
-                continue
-            countWrongItem = 0;
-#           We get all the column of the category in the CSV file
-            dataFeature = self.fileCSV[cat_name]
-#            We make a dictionnary to be granted to put string keys
-            feature =  collections.OrderedDict()
-
-            for index in dataFeature:
-                if "?" in index:
-                    countWrongItem = countWrongItem + 1 
-                    
-#           Put in the table feature the name of the feature
-            feature['nameFeature'] = cat_name
-#           Put in the table feature the total count of lines
-            feature['countTotal'] = dataFeature.size
-                   
-            feature['% Miss'] = countWrongItem/ dataFeature.size * 100
-            print(feature['% Miss'])
-            feature['cardTotal'] = np.unique(dataFeature).size
+            pd.DataFrame(table).to_csv(path_or_buf= path, header= header_columns, index= False);
     
-            feature['First Mode'] = dataFeature.value_counts().keys()[0]
-            feature['First Mode Freq'] = dataFeature.value_counts()[0]
-            feature['First Mode %'] = round(dataFeature.value_counts()[0] / dataFeature.size * 100,2)
-            
-            feature['Second Mode'] = dataFeature.value_counts().keys()[1]
-            feature['Second Mode Freq'] = dataFeature.value_counts()[1]
-            feature['Second Mode %'] = round(dataFeature.value_counts()[1] / dataFeature.size * 100,2)
-            self.__categorical_features_table.append(feature)   
+
+
+    
+    def draw_DQR(self):
+        print("here")
+        self.__all_categorical_header = [];
+        self.__continuous_features_table = [];
+        self.__categorical_header = ["Feature name","Count","% Miss", "Card", "Mode", "Mode Freq", "Mode %","2nd Mode","2nd Mode Freq","2nd Mode %"]
+        continuous_columns = self.categorical;
+        self.__all_categorical_table = collections.OrderedDict();
+        for col, cat in enumerate(continuous_columns.keys()):
+            print(col, cat)
+            dataFeature = self.fileCSV[cat];
+            hasInt = False;
+            countWrongItem = 0;
+
+            for value in dataFeature:
+                if type(value) is int:
+                    print(value)
+                    hasInt = True
+                elif "Not in universe" in value:
+                    countWrongItem = countWrongItem + 1 
+            if hasInt is False :
+                
+                feature = collections.OrderedDict();
+#           Put in the table feature the name of the feature
+                feature['nameFeature'] = cat
+#           Put in the table feature the total count of lines
+                feature['countTotal'] = dataFeature.size
+                feature['% Miss'] = countWrongItem/ dataFeature.size * 100
+                feature['cardTotal'] = np.unique(dataFeature).size
         
-#           Write continuous table
-        self.write_results();
-#           Write continuous features table
-        self.write_results(self.__categorical_features_table,categorical_header, self.pathDQR);
+#           check 
+                iFirstMode= 0;
+                while("Not in universe" in dataFeature.value_counts().keys()[iFirstMode] or "?" in dataFeature.value_counts().keys()[iFirstMode]):
+                    iFirstMode = iFirstMode + 1
+            
+                iSecondMode= iFirstMode + 1;
+                while("Not in universe" in dataFeature.value_counts().keys()[iSecondMode] or "?" in dataFeature.value_counts().keys()[iSecondMode]):
+                    iSecondMode = iSecondMode + 1
+                    
+                feature['First Mode'] = dataFeature.value_counts().keys()[iFirstMode]
+                feature['First Mode Freq'] = dataFeature.value_counts()[iFirstMode]
+                feature['First Mode %'] = round(dataFeature.value_counts()[iFirstMode] / dataFeature.size * 100,2)
+                
+                feature['Second Mode'] = dataFeature.value_counts().keys()[iSecondMode]
+                feature['Second Mode Freq'] = dataFeature.value_counts()[iSecondMode]
+                feature['Second Mode %'] = round(dataFeature.value_counts()[iSecondMode] / dataFeature.size * 100,2)
+                self.__continuous_features_table.append(feature);
+                
+                self.__all_categorical_header.append(cat);
+                self.__all_categorical_table[cat] = continuous_columns.values[:,col];
+              
+#        Write DQR continuous
+        self.write_results(self.__continuous_features_table, self.__categorical_header, self.pathDQR);
+        self.write_results(self.__all_categorical_table, self.__all_categorical_header,"./results/categorical-features.csv");
+
+#cont = Continuous();
+#cont.draw_DQR()
--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import continuous as cont;
-import categoricalv2 as cat;
+import categorical as cat;
 import utils as u;
-import view as v;
 
 def main():
     
@@ -13,20 +12,17 @@ def main():
 #        f.write("age,class of worker,detailed industry recode,detailed occupation recode,education,wage per hour,unroll in edu inst last week,marital status,major industry code,major occupation code,race,hispanic origin,sex,member of a labor union,reason for unemployment,full or part time employment stat,capital gains,capital losses,dividends from stocks,tax filter stat,region of previous residence,state of previous residence,detailed household and family stat,detailed household summary in household,instance weight,migration code-change in MSA,migration code-change in REG,migration code-change within REQ,live in this house 1 year ago,migration prev res in sunbelt,num person work for employer,family member under 18,country of birth father,country of birth mother,country of birth self,citizenship,own business or self employed,fill inc questionnaire for veteran's admin,veteran's benefits,weeks work in year,year")
 #        f.write(raw_csv)
         
-    #continuous = cont.Continuous(file);
-    #continuous.draw_DQR();
+    continuous = cont.Continuous(file);
+    continuous.draw_DQR();
 
 
     categorical = cat.CategoricalV2(file);
     categorical.draw_DQR();
     
-#    categorical = cat.Categorical('./dataset/census-income.data.csv');    
-#    categorical.draw_DQR();
+    utils = u.Utils();
     
-#    view = v.View();
-#    
-#    view.graph_continuous();
-#    view.graph_categorical();
+    utils.graph_continuous();
+    utils.graph_categorical();
 
     print("Finished");
 
